2c9e1d7a0bc198bf38107f90d6f44739c3980623
Aman kumar <92974468+AMANKUMAR1020@users.noreply.github.com>
2024-12-09 17:07:44 +0530

    Update 2337_Move_Pieces_to_Obtain_a_String.cpp

--- leetcode/2337_Move_Pieces_to_Obtain_a_String.cpp
+++ leetcode/2337_Move_Pieces_to_Obtain_a_String.cpp
@@ -34,3 +34,46 @@ public:
         return true;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+//other method 
+
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int n = start.size();
+        queue<pair<char, int>> startq, targetq;
+
+        // Collect characters and their indices in both strings
+        for (int i = 0; i < n; i++) {
+            if (start[i] != '_') startq.push({start[i], i});
+            if (target[i] != '_') targetq.push({target[i], i});
+        }
+
+        // If the number of characters in both queues does not match, return false
+        if (startq.size() != targetq.size())
+            return false;
+
+        // Validate movements
+        while (!startq.empty()) {
+            auto [val1, ind1] = startq.front(); startq.pop();
+            auto [val2, ind2] = targetq.front(); targetq.pop();
+
+            // Check if characters and their relative positions are valid
+            if (val1 != val2) return false; // Characters must match
+            if (val1 == 'L' && ind1 < ind2) return false; // 'L' can only move left
+            if (val1 == 'R' && ind1 > ind2) return false; // 'R' can only move right
+        }
+
+        return true;
+    }
+};
