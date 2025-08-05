9a9f91c530b4857b454921225c007d7654583d0b
Aman kumar <92974468+AMANKUMAR1020@users.noreply.github.com>
2025-08-05 20:46:58 +0530

    Update 42_Trapping_Rain_Water.cpp

--- leetcode/42_Trapping_Rain_Water.cpp
+++ leetcode/42_Trapping_Rain_Water.cpp
@@ -35,3 +35,54 @@ public:
 }
 
 };
+
+
+
+
+
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int ans = 0;
+        int l = 0, r = height.size()-1;
+        int maxL = height[l];
+        int maxR = height[r];
+
+        while(l<r){
+            if(maxL < maxR){
+                ans += maxL - height[l];
+                maxL = max(maxL, height[++l]);
+            }else{
+                ans += maxR - height[r];
+                maxR = max(maxR, height[--r]);
+            }
+        }
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int ans = 0;
+        vector<int> l(n);
+        vector<int> r(n);
+
+        for(int i=0; i<n; i++)
+            l[i] = i==0 ? height[i] : max(height[i], l[i-1]);
+
+        for(int i=n-1; i>=0; i--)
+            r[i] = i==n-1 ? height[i] : max(height[i], r[i+1]);
+
+        for(int i=0; i<n; i++)
+            ans += min(l[i], r[i]) - height[i];
+        
+        return ans;
+    }
+};
+
