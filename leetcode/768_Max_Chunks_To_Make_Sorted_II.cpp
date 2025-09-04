cb87da335d8518720a4616ef90e5a056eab1094d
Aman kumar <92974468+AMANKUMAR1020@users.noreply.github.com>
2025-09-04 10:21:58 +0530

    Create 768_Max_Chunks_To_Make_Sorted_II.cpp

---
+++ leetcode/768_Max_Chunks_To_Make_Sorted_II.cpp
@@ -0,0 +1,22 @@
+// this question is same as "768. Max Chunks To Make Sorted II"
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int cnt = 0, n = arr.size();
+		stack<int> st;
+		for(int i=0; i<n; i++){
+			if(st.empty() || st.top() < arr[i]){
+				st.push(arr[i]);
+			}else{
+				int temp = st.top();
+				while(!st.empty() && arr[i] < st.top()){
+					st.pop();
+				}
+				st.push(temp);
+			}
+		}
+		
+		return st.size();
+
+    }
+};
