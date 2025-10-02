974ad3bf02dc7cd10ffef8d0b83a05a90d3f9fa4
Aman kumar <92974468+AMANKUMAR1020@users.noreply.github.com>
2025-10-02 17:36:08 +0530

    Create Linked_List_Insertion_At_End.cpp

---
+++ geeksforgeeks/Linked_List_Insertion_At_End.cpp
@@ -0,0 +1,37 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        Node* temp = head;
+        Node* curr = temp;
+        Node* newNode = new Node(x);
+
+        if(head == nullptr)
+            return newNode;
+            
+        while(curr->next != nullptr)
+            curr = curr->next;
+        curr->next = newNode;
+        
+        head = temp;
+        return head;
+    }
+};
