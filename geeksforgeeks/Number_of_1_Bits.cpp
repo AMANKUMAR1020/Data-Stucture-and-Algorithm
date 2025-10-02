f787807ec1846d5ebf8bd7439aacf5bcd15b8957
Aman kumar <92974468+AMANKUMAR1020@users.noreply.github.com>
2025-10-02 20:54:46 +0530

    Create Number_of_1_Bits.cpp

---
+++ geeksforgeeks/Number_of_1_Bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int setBits(int n) {
+        int cnt=0;
+        while(n){
+            if(n&1)
+                cnt++;
+            n = n >> 1;
+        }
+        return cnt;
+    }
+};
