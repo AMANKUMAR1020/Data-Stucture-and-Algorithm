77a7914b9f037d42640e7c7f0baddd1b1d07b221
Aman kumar <92974468+AMANKUMAR1020@users.noreply.github.com>
2025-10-20 14:43:07 +0530

    Update Remove_loop_in_Linked_List.cpp

--- geeksforgeeks/Remove_loop_in_Linked_List.cpp
+++ geeksforgeeks/Remove_loop_in_Linked_List.cpp
@@ -1,10 +1,49 @@
-class Solution {
-public:
-    int finalValueAfterOperations(vector<string>& operations) {
-        int ans = 0;
-        for(string& str : operations){
-            ans += (str == "--X" || str == "X--") ? -1 : 1;
-        }
-        return ans;
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
     }
 };
+*/
+class Solution {
+  public:
+    void removeLoop(Node* head) {
+        Node* curr = head;
+        Node* temp = curr;
+        unordered_map<Node*, bool> mp;
+        mp[curr]=true;
+        while(curr->next != nullptr && mp.find(curr->next) == mp.end()){
+            mp[curr->next] = true;
+            curr = curr->next;
+        }
+        curr->next = nullptr;
+    }
+};
+
+
+/*
+class Solution {
+  public:
+    void removeLoop(Node* head) {
+        Node* curr = head;
+        Node* temp = curr;
+        unordered_map<Node*, bool> mp;
+        
+        while(curr->next != nullptr){
+            if(!mp.empty() && mp.find(curr->next) != mp.end()){
+                break;
+            }else{
+                mp[curr->next] = true;
+            }
+            curr = curr->next;
+        }
+        curr->next = nullptr;
+        head = temp;
+    }
+};
+*/
