bb38510286c76a51026f267967d92dcaa3feeb30
Aman kumar <92974468+AMANKUMAR1020@users.noreply.github.com>
2025-11-25 23:52:24 +0530

    Create 1015_Smallest_Integer_Divisible_by_K.cpp

---
+++ leetcode/1015_Smallest_Integer_Divisible_by_K.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int smallestRepunitDivByK(int K) {
+        int remainder = 0;
+        for (int length_N = 1; length_N <= K; length_N++) {
+            remainder = (remainder * 10 + 1) % K;
+            if (remainder == 0) {
+                return length_N;
+            }
+        }
+        return -1;
+    }
+};
